abf2c044e79f97c5f86063cc0e6e47c78302b783
Aziz1019 <starinc1998@gmail.com>
2022-04-27 10:39:48 +0500

    Bot file changes

--- bot.py
+++ bot.py
@@ -1,2 +1,35 @@
-print("Hello World")
+"""
+This is an echo bot.
+It echoes any incoming text messages.
+"""
 
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '5219586926:AAHZdk7HgBHGKIILCqtqJAUFbociulB4nyQ'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Botga Hush Kelibsiz!")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
